37cc8a2ef7af266527a70b6daa79a9bf709048f5
MaximBabenkov <lostbass@mail.ru>
2022-06-13 16:19:55 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать треугольник Паскаля
+Console.Clear();
+Console.Write("Enter your number of rows: ");
+int number=int.Parse(Console.ReadLine()??"0");
+Console.WriteLine();
+for (int y=0;y<number;y++)
+{         
+    int count=1;
+    for (int q=0;q<number-y;q++)
+    {
+        Console.Write("   ");
+    }
+    for (int x=0;x<=y;x++)
+    {
+        Console.Write("   {0:d} ",count);
+        count=count*(y - x)/(x + 1);
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.WriteLine();
